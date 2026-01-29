c385d79e6affd5f53354b3a3910c65dcb552aeec
Erick Betancourt <ebetancourt@drg.mx>
2026-01-29 15:57:41 -0600

    Agregado welcome de DRG para proyecto inicial de almacen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!Stringable::hasMacro('doesntStartWith')) {
+        Stringable::macro('doesntStartWith', function ($needles) {
+            return ! Str::startsWith($this->value, $needles);
+        });
+    }
     }
 }
