a72b7f1e998ddff4e13be102f828246ccd4408f7
Erick Betancourt <ebetancourt@drg.mx>
2026-01-29 13:07:33 -0600

    Agregada URL de prueba de API para webhook

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GitDeployController;
 
 Route::post('/git-deploy', [GitDeployController::class, 'deploy']);
+
+Route::get('/test-api', function () {
+    return response()->json(['message' => 'API operativa para irisDRG']);
+});
