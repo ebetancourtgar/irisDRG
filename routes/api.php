fad6d988ddf3956cbd47701893e17f1075fcf58e
Erick Betancourt <ebetancourt@drg.mx>
2026-01-29 15:43:13 -0600

    Ajuste en archivo de rutas de API

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GitDeployController;
 
 Route::post('/git-deploy', [GitDeployController::class, 'deploy']);
-
-Route::get('/test-api', function () {
-    return response()->json(['message' => 'API operativa para irisDRG']);
-});
