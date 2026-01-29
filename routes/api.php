6dee6794cc96b624a962cb3baf9fc081a86bb8ce
Erick Betancourt <ebetancourt@drg.mx>
2026-01-29 15:44:29 -0600

    Agregado comentario de prueba

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GitDeployController;
 
 Route::post('/git-deploy', [GitDeployController::class, 'deploy']);
+
+// Other API routes can be defined here
