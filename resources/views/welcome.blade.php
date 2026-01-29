0a7b20c3fb45179e7fa6591de250a416770b7486
Erick Betancourt <ebetancourt@drg.mx>
2026-01-29 16:54:54 -0600

    Compilados assets npm run build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -179,6 +179,7 @@
                                      class="w-64 h-auto mx-auto">
                             </div>
 
+                            
                             {{-- Badges flotantes --}}
                             <div class="absolute -top-4 -right-4 bg-green-500 text-white px-4 py-2 rounded-full text-sm font-bold shadow-lg animate-float">
                                 <div class="flex items-center space-x-2">
