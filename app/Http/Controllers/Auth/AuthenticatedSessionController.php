0dd8791c390c7c9d6d90e25cb7e402ffc0dea09b
Erick Betancourt <ebetancourt@drg.mx>
2026-01-29 22:27:53 -0600

    ajustes en gitingnore para subir assets compilados en local

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -42,6 +42,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect(route('dashboard.index', absolute: false));
+        return redirect(route('welcome', absolute: false));
     }
 }
